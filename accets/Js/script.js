a65099ee9a6edbdf84f09179c2e8490126069418
BuiThanh <thanhphuongmao4@gmail.com>
2023-03-09 09:24:47 +0700

    first commit

---
+++ accets/Js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 4000,
+    },
+    effect: 'cube'
+    // If we need pagination
+    
+  });
